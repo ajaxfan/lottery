ef68942ff46692eadc57877a652d4d8381a51ac5
ajaxfan <ajax_2003@163.com>
2016-06-29 19:20:37 +0800

    修正了POM文件，使用Parent替代了繁琐的依赖

---
+++ lotterdata/src/main/java/com/xpyz/lotterdata/Application.java
@@ -0,0 +1,9 @@
+package com.xpyz.lotterdata;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-*.xml");
+	}
+}
---
+++ lotterdata/target/start.bat
@@ -0,0 +1,2 @@
+java -jar E:\Repository\lottery\lotterdata\target\lotterdata-1.0.0.jar
+pause
